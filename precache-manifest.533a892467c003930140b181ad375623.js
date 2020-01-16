85164b0529742955dad21bda3898d7247c00d3c5
Tom Lodge <tlodge@gmail.com>
2020-01-16 17:07:33 +0000

    updated

---
+++ precache-manifest.533a892467c003930140b181ad375623.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "937d4836265f53567db25194cf21ad4c",
+    "url": "/kaplanparcelswebsite/index.html"
+  },
+  {
+    "revision": "7304e30078d9dcde791c",
+    "url": "/kaplanparcelswebsite/static/css/main.0e05c655.chunk.css"
+  },
+  {
+    "revision": "c65e6a8a1cdea80c315c",
+    "url": "/kaplanparcelswebsite/static/js/2.1318af3d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kaplanparcelswebsite/static/js/2.1318af3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7304e30078d9dcde791c",
+    "url": "/kaplanparcelswebsite/static/js/main.2814d5ce.chunk.js"
+  },
+  {
+    "revision": "15b96bcf9d15f5d104c6",
+    "url": "/kaplanparcelswebsite/static/js/runtime-main.5ece8d15.js"
+  }
+]);
