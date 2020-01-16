3c9f7ae89ef712f9ed7ac7c8a5ab876de299553c
Tom Lodge <tlodge@gmail.com>
2020-01-16 16:59:57 +0000

    first commit

---
+++ precache-manifest.7ee22b87e6834637fef278ebaa600ce5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd010acd9f934727cd9011dc01b78367",
+    "url": "/kaplanparcelwebsite/index.html"
+  },
+  {
+    "revision": "2a1bc46ce43b35a478e1",
+    "url": "/kaplanparcelwebsite/static/css/main.0e05c655.chunk.css"
+  },
+  {
+    "revision": "c65e6a8a1cdea80c315c",
+    "url": "/kaplanparcelwebsite/static/js/2.1318af3d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/kaplanparcelwebsite/static/js/2.1318af3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a1bc46ce43b35a478e1",
+    "url": "/kaplanparcelwebsite/static/js/main.ec803929.chunk.js"
+  },
+  {
+    "revision": "2547b8a6ba30e7086846",
+    "url": "/kaplanparcelwebsite/static/js/runtime-main.5004bb59.js"
+  }
+]);
