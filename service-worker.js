e4396c062868a4504a4be60b829b98e99d2eb95e
Tom Lodge <tlodge@gmail.com>
2020-01-16 17:11:03 +0000

    cp commit

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/kaplanparcelwebsite/precache-manifest.7ee22b87e6834637fef278ebaa600ce5.js"
+  "/kaplanparcelswebsite/precache-manifest.533a892467c003930140b181ad375623.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kaplanparcelwebsite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/kaplanparcelswebsite/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
